0c2d9dcd28243f7f5f0b1ce5096e0038ab41c0c9
Marie Frostova <59430302+marie-frostova@users.noreply.github.com>
2022-02-27 23:04:13 +0700

    Update Pokemon.cs

--- src/PokeApi/Models/Pokemon.cs
+++ src/PokeApi/Models/Pokemon.cs
@@ -1,4 +1,4 @@
-﻿using System.Text.Json.Serialization;
+using System.Text.Json.Serialization;
 
 namespace PokeApi.Models
 {
@@ -9,7 +9,8 @@ namespace PokeApi.Models
 
         [JsonPropertyName("name")]
         public string Name { get; set; }
-        
+
+        [JsonPropertyName("height")]
         public int Height { get; set; }
 
         [JsonPropertyName("types")]
