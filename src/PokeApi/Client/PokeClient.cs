2925bbc36977c9943763b048ce1f3c16c4bef2e2
Sergey Suchkov <suseal@inbox.ru>
2022-02-27 22:12:03 +0700

    update PokeClient, fix Pokemon and PokemonList

--- src/PokeApi/Client/PokeClient.cs
+++ src/PokeApi/Client/PokeClient.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net.Http;
+using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 using System.Web;
@@ -29,12 +30,39 @@ namespace PokeApi.Client
 
         public async Task<Pokemon> GetPokemonAsync(int id)
         {
-            throw new NotImplementedException();
+            var response = await this.httpClient.GetAsync($"pokemon/{id}");
+
+            var content = await response.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<Pokemon>(content);
         }
 
         public async Task<PokemonList> GetPokemonsAsync(int? offset = null, int? limit = null)
         {
-            throw new NotImplementedException();
+            var response = await this.httpClient.GetAsync(GetRequest(offset, limit));
+
+            var content = await response.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<PokemonList>(content);
+        }
+
+        private static string GetRequest(int? offset = null, int? limit = null)
+        {
+            var requestBuilder = new StringBuilder();
+            requestBuilder.Append("pokemon");
+
+            var offsetParam = offset == null ? "" : $"offset={offset}";
+            var limitParam = limit == null ? "" : $"limit={limit}";
+                        
+            if (offset != null || limit != null)
+                requestBuilder.Append("?");
+
+            requestBuilder.Append(offsetParam);
+
+            if (offset != null && limit != null)
+                requestBuilder.Append("&");
+
+            requestBuilder.Append(limitParam);
+
+            return requestBuilder.ToString();
         }
     }
 }
